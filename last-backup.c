bcd41b5973237cf5d9a25de851e837d12cf6524e
dovydas55 <dovydas13@ru.is>
2015-03-12 14:59:18 +0000

    cleaning and backup

--- last-backup.c
+++ last-backup.c
@@ -212,7 +212,7 @@ void *mm_malloc(size_t size)
 
     } else { //in a free list    
 	place(p, newsize);
-	remove_block(p, newsize);
+
     }
     
     void *result = (void *)((char *)p + NODE_PTR_SIZE);
@@ -345,7 +345,7 @@ static void place(void *bp, size_t asize)
   if ((csize - asize) >= (DSIZE + OVERHEAD)) { 
     PUT(HDRP(bp), PACK(asize, 1));
     PUT(FTRP(bp), PACK(asize, 1));
-    
+    remove_block((NodePtr*)bp, csize);
     bp = NEXT_BLKP(bp);
     PUT(HDRP(bp), PACK(csize-asize, 0));
     PUT(FTRP(bp), PACK(csize-asize, 0));
@@ -354,6 +354,7 @@ static void place(void *bp, size_t asize)
   else { 
     PUT(HDRP(bp), PACK(csize, 1));
     PUT(FTRP(bp), PACK(csize, 1));
+    remove_block((NodePtr*)bp, csize);
   }
 }
 /* $end mmplace */
